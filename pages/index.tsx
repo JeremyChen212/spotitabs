41ee8fcdd7b3e77de8d7a9d1df8def40accebc7c
JeremyChen212 <50683386+JeremyChen212@users.noreply.github.com>
2023-04-01 21:47:20 -0700

    imported playlistdashboard to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,46 @@
 import {useSession, signIn, signOut} from 'next-auth/react';
+import { useRouter } from 'next/router';
 import { Container, Center, Image, Button } from '@chakra-ui/react'
 import * as Popover from '@radix-ui/react-popover';
 import SpotitabsLogo from '/public/Spotitabs_Logo.jpg'
-import requireSpotifyAuth from "../middleware";
+import PlaylistDashboard from '../components/PlaylistDashboard';
+import { getSession } from "next-auth/react";
+import { getUsersPlaylists } from '../lib/spotify'
+import { GetServerSideProps } from "next";
+import { useEffect } from 'react';
+import { useSpotify } from '../context/SpotifyContext'
+import { customGet } from '@component/utils/customGet';
 
-export default function Home() {
+function Home( {} ) {
   const {data: session} = useSession();
+  const {status: loading} = useSession();
+  const router = useRouter();
+
   const handleLogin = () => {
-    signIn("spotify", { callbackUrl: "http://localhost:3000" });
+    signIn("spotify", { callbackUrl: "http://localhost:3000/" });
   };
+  
+  useEffect(() => {
+    // Redirect to login page if session is expired or missing tokens
+    if (!loading && (!session || !session.accessToken || !session.refreshToken)) {
+      router.push('/login');
+    }
+  }, [session, loading, router]);
+    
+  // Redirect to home page after logging in
+  useEffect(() => {
+    if (!loading && session && session.accessToken && session.refreshToken) {
+      router.push('/');
+    }
+  }, [session, loading, router]);
+
 
   if (session) {
     return (
-      <div className="flex flex-col items-center justify-center w-screen h-screen gap-20 bg-[background1] text-white">
+      <div className="flex flex-col items-center justify-center gap-20 text-white">
         Signed in as {session?.token?.email} <br />
         <button onClick={() => signOut()}>Sign out</button>
+        <PlaylistDashboard ></PlaylistDashboard>
       </div>
     );
   } 
@@ -33,3 +59,30 @@ export default function Home() {
   </div>
   )
 }
+
+// export const getServerSideProps: GetServerSideProps = async (ctx) => {
+//   const session = await getSession(ctx);
+
+//   if (!session) {
+//     return {
+//       redirect: {
+//         destination: "/login",
+//         permanent: false,
+//       },
+//     };
+//   }
+
+//   const newReleases = await customGet(
+//     "https://api.spotify.com/v1/browse/new-releases?country=IN&limit=25",
+//     session
+//   );
+
+//   const featuredPlaylists = await customGet(
+//     "https://api.spotify.com/v1/browse/featured-playlists?country=IN",
+//     session
+//   );
+
+//   return { props: { newReleases, featuredPlaylists } };
+// };
+
+export default Home
