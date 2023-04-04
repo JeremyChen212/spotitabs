66629089c069d582b74eed1cfe4618d03ca0b3b5
JeremyChen212 <50683386+JeremyChen212@users.noreply.github.com>
2023-04-04 10:11:53 -0700

    playlist endpoint with customGet

--- pages/api/playlists.ts
+++ pages/api/playlists.ts
@@ -1,16 +1,17 @@
-import {getUsersPlaylists} from '../../lib/spotify';
-import {getSession} from 'next-auth/react';
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/react";
+import { customGet } from "../../utils/customGet";
 
-const handler = async (req, res) => {
-    const {
-      token: {accessToken},
-    } = await getSession({req});
-    const response = await getUsersPlaylists(accessToken);
-    const {items} = await response.json();
-  
-    return res.status(200).json({items});
-  };
-  
-  export default handler;
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const session = await getSession({ req });
 
-  
+  const playlists = await customGet(
+    "https://api.spotify.com/v1/me/playlists",
+    session
+  );
+
+  res.status(200).json(playlists);
+}
