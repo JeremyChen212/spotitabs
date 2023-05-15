18e3d84acf67909416219241a3f01d343b1c954d
JeremyChen212 <50683386+JeremyChen212@users.noreply.github.com>
2023-05-14 18:39:14 -0700

    cool search  submit button

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,18 +1,28 @@
-import { IoSearch } from 'react-icons/io5';
+import { IoSearch, IoArrowForward, IoCaretForward } from 'react-icons/io5';
 import { useState, useRef } from "react";
 import { useSpotify } from "@component/context/SpotifyContext";
+import { useRouter } from 'next/router';
 
 
 export default function Searchbar() {
+    const router = useRouter();
     const searchInput = useRef(null)
     const {popupActive, setPopupActive} = useSpotify()
     const [searchQuery, setSearchQuery] = useState("")
+    function handleSearch(event) {
+        event.preventDefault();
+        router.push(`/search?q=${encodeURIComponent(searchQuery)}`);
+    }
     return (
-        <div className="w-full bg-[#FFFFFF] my-4 rounded-md relative">
-                <IoSearch className="text-[#17191A] left-0 ml-4 stroke-[1rem] text-xl absolute pointer-events-none top-[50%] translate-y-[-50%]"></IoSearch>
+        <form onSubmit={(e)=>handleSearch(e)} className="w-full bg-[var(--bg-2-color)] text-[white] my-4 rounded-md relative">
+                <IoSearch className="text-[#ffffff6a] left-0 ml-4 stroke-[1rem] text-xl absolute pointer-events-none top-[50%] translate-y-[-50%] "></IoSearch>
                 <input id="searchInput" type="text" onChange={e => setSearchQuery(e.target.value)} value={searchQuery} 
-                className="outline-none bg-[#ffffff00] text-[#17191A] w-full p-3 pl-12" placeholder="Find a song, artist, album, or playlist"/>
+                className="peer outline-none bg-[#ffffff00] w-full p-3 pl-12" placeholder="Find a song, artist, album, or playlist"/>
+                <button type="submit" className="text-[#17191A] absolute top-[50%] translate-y-[-50%] right-0 opacity-0 mr-2 h-8 w-8 flex items-center justify-center rounded-md bg-[#A4FDD3] peer-focus:opacity-100 transition-all overfow-hidden group">
+                    <IoCaretForward className="transition-[inherit] group-hover:translate-x-[0.3rem]"></IoCaretForward>
+                    <IoCaretForward className='transition-[inherit] absolute left-[-1rem] opacity-0 group-hover:left-[0.3rem] group-hover:opacity-100'></IoCaretForward>
+                </button>
                 {/* <hr className="opacity-20 z-3"/> */}
-        </div>
+        </form>
     )
 }
