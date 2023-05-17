732b3ade8c3457ba81172a5579420c7bd59854a9
JeremyChen212 <50683386+JeremyChen212@users.noreply.github.com>
2023-05-16 19:14:19 -0700

    [Component] - song card row UI

--- components/reusable/SongCard.tsx
+++ components/reusable/SongCard.tsx
@@ -1,37 +1,39 @@
-import Image from "next/image";
-import Link from "next/link";
-import { useState } from "react";
+import React from 'react';
+import Image from 'next/image';
+const SongCard = ({ song }) => {
+    const formatDuration = (durationMs) => {
+        const minutes = Math.floor(durationMs / 60000);
+        const seconds = Math.floor((durationMs % 60000) / 1000);
+        return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+      };
+    const duration = formatDuration(song.duration_ms);
 
 
-export default function songCard({ song }: any) {
-  // Get all the <a> tags on the page
-  const [isLoading, setIsLoading] = useState(true);
-  const [hasError, setHasError] = useState(false);
-
-  const handleLoadComplete = () => {
-    setIsLoading(false);
-  };
-
-  const regex = /(<([^>]+)>)/ig;
-
+//   const { name, artist, album, duration, coverImage } = song;
   return (
-    <>
-     <Link href={`/song/${song.id}`} className="flex flex-col relative cursor-pointer  hover:scale-[1.01] transition-all rounded-lg h-fit overflow-hidden w-full gap-4">
-      <div className={`w-full animated-border-2 rounded-lg overflow-hidden`}>
-          <Image 
-            onLoadingComplete={handleLoadComplete}
-            unoptimized={true}
-            loader={()=>song?.images?.[0]?.url}
-            src={song?.images?.[0]?.url} width={112} height={112}  alt="Song Image" className="h-full w-full object-cover" priority/>
-        </div> 
-          <div className="w-full flex-1">
-            <h2 className="text-lg font-medium mb-[0.15rem] w-full line-clamp-1 text-ellipse">{song.name}</h2>
-            <div className="text-[#A9A9A9] pointer-events-none line-clamp-2 text-ellipse">
-                by {song?.owner.display_name}
-            </div>
-          </div>
-       
-     </Link>
-    </>
-  )
+    <div className="grid max-sm:grid-cols-8 grid-cols-12 bg-[var(--bg-2-color)] cursor-pointer p-2 px-4 rounded-lg items-center text-start w-full justify-between">
+        <div className="flex col-span-6 items-center text-start space-x-4">
+        <Image 
+                unoptimized={true}
+                loader={()=>song?.images?.[0]?.url}
+                src={song?.album?.images?.[0]?.url} width={112} height={112}  alt="Song Image" className="shadow-md w-16 h-16 rounded-md" 
+                priority/>
+        <div>
+            <h3 className="text-lg font-bold">{song.name}</h3>
+            <p className="text-sm text-gray-500">
+                {song.artists.map((artist, index) => (
+                    <React.Fragment key={index}>
+                        { (index ? ', ' : '' ) } 
+                        {artist.name}
+                    </React.Fragment>
+                ))}
+            </p>
+        </div>
+    </div>
+    <p className="text-sm max-sm:hidden col-span-4 text-gray-500">{song?.album?.name}</p>
+    <p className="text-sm justify-self-end col-span-2 text-gray-500">{duration}</p>
+    </div>
+  );
 };
+
+export default SongCard;
