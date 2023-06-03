dc28b50eb2845324c211260af70c12dd5621d496
JeremyChen212 <50683386+JeremyChen212@users.noreply.github.com>
2023-06-03 00:21:37 -0700

    playlist sidebar scroll option 2

--- components/MainPage/PlaylistSidebar.tsx
+++ components/MainPage/PlaylistSidebar.tsx
@@ -46,40 +46,39 @@ export default function PlaylistSidebar({playlist}) {
       const ptag = playlistSmallTextRef.current
       const dist = contentElement.offsetWidth - htag.offsetWidth
       document.getElementById("smallScrollTop")!.style.width = `${contentElement.offsetWidth}px`;
-      document.getElementById("playlistTop")!.style.width = `${contentElement.offsetWidth}px`;
+      // document.getElementById("playlistTop")!.style.width = `${contentElement.offsetWidth}px`;
       if(contentElement && bigTopElement) {
         document.getElementById("sidebarContainer")?.addEventListener("scroll", function() {
           const initialSidebarTop = parentDivElement.getBoundingClientRect().top;
           let actualSelectedElTop = contentElement.getBoundingClientRect().top;
           let selectedElPosition = (actualSelectedElTop - initialSidebarTop);
           console.log(selectedElPosition)
-          document.getElementById("playlistTop")!.style.height = `${selectedElPosition}px`;
-          document.getElementById("playlistTop")!.style.width = `${contentElement.offsetWidth}px`;
+          // document.getElementById("playlistTop")!.style.height = `${selectedElPosition}px`;
+          // document.getElementById("playlistTop")!.style.width = `${contentElement.offsetWidth}px`;
           document.getElementById("smallScrollTop")!.style.width = `${contentElement.offsetWidth}px`;
         })
       }
       const ctx = gsap.context((self) => {
-        gsap.from(bigTopElement, {
-          opacity: 1,
-          x: 0,
-        });
         gsap.from(smallTopDivElement, {
           opacity: 0,
           x: 0,
         });
-        
-        gsap.to(bigTopElement, {
-          ease: "none",
-          opacity: 0,
-          duration: 1000,
-          scrollTrigger: {
-            trigger: "#playlistTop",
-            start: "top top",
-            end: '+=50',
-            scrub: true,
-            scroller: "#sidebarContainer",
-          },
+        gsap.from(smallTopDivElement.children, {
+          opacity: 0
         });
+        
+        // gsap.to(bigTopElement, {
+        //   ease: "none",
+        //   opacity: 0,
+        //   duration: 1000,
+        //   scrollTrigger: {
+        //     trigger: "#playlistTop",
+        //     start: "top top",
+        //     end: '+=50',
+        //     scrub: true,
+        //     scroller: "#sidebarContainer",
+        //   },
+        // });
        
         // gsap.to(playlistTextElement, {
         //   ease: "none",
@@ -111,7 +110,19 @@ export default function PlaylistSidebar({playlist}) {
           duration: 1000,
           scrollTrigger: {
             trigger: "#sidebarContainer",
-            start: "top+=200",
+            start: "top+=50",
+            end: '+=50',
+            scrub: true,
+            scroller: "#sidebarContainer",
+          },
+        });
+        gsap.to(smallTopDivElement.children, {
+          opacity: 1,
+          ease: "none",
+          duration: 1000,
+          scrollTrigger: {
+            trigger: "#sidebarContainer",
+            start: "top+=150",
             end: '+=50',
             scrub: true,
             scroller: "#sidebarContainer",
@@ -170,7 +181,8 @@ export default function PlaylistSidebar({playlist}) {
             <div ref={parentDivRef} id="sidebarContainer"  className={`bg-[var(--bg1)]  flex flex-col gap-6 h-full overflow-y-scroll rounded-md max-w-md w-[50rem] pr-4 relative`}>
                 <div
                   ref={bigTopRef}
-                  id="playlistTop"  className={`min-h-[4rem] flex h-fit pl-2 overflow-hidden gap-6 items-start fixed bg-[var(--bg1)] py-4 rounded-md z-20 `}>
+                  // id="playlistTop"  className={`flex h-fit pl-2 overflow-hidden gap-6 items-start fixed bg-[var(--bg1)] py-4 rounded-md z-20 `}>
+                  id="playlistTop"  className={`flex h-fit pl-2  gap-6 items-start py-4 rounded-md z-30 `}>
                   <div className="p-2 bg-[var(--bg3)] cursor-pointer transition-all ease-in-out hover:brightness-200 absolute rounded-full right-4 top-4 rotate-180">
                     <IoArrowForward className="text-lg text-gray-300"></IoArrowForward>
                   </div>
@@ -191,18 +203,18 @@ export default function PlaylistSidebar({playlist}) {
                         {playlist.name}
                     </h1>
                     <p 
-                    ref={playlistSmallTextRef}
                     className="text-gray-300">
                       {playlist.owner.display_name}
                     </p>
                   </div>
                 </div>
 
-                <div ref={smallTopDivRef} id="smallScrollTop" className={`opacity-0 overflow-hidden flex pl-2 gap-6 items-start fixed py-4 rounded-md z-30`}>
+                <div ref={smallTopDivRef} id="smallScrollTop" className={`opacity-0  overflow-hidden flex pl-2 gap-6 items-start fixed py-4 rounded-md z-30`}>
                   <div ref={smallTopTextRef} className="p-2 bg-[var(--bg3)] cursor-pointer transition-all ease-in-out hover:brightness-200 absolute rounded-full right-4 top-[50%] translate-y-[-50%] rotate-180">
                     <IoArrowForward className="text-lg text-gray-300"></IoArrowForward>
                   </div>
                   <div
+                    ref={playlistSmallTextRef}
                     id="playlistText"
                   className="line-clamp-2 pl-4 text-ellipse  w-full pr-12 text-start flex flex-col gap-2">
                     <h1 className="text-[1rem] font-semibold leading-[2rem] ">
@@ -211,7 +223,7 @@ export default function PlaylistSidebar({playlist}) {
                   </div>
                 </div>
 
-                <div id="sidebarContent" className="mt-[10rem] z-10 h-fit" ref={contentRef}>
+                <div id="sidebarContent" className="z-10 h-fit" ref={contentRef}>
                   <hr className="opacity-10" />
                   <div className="">
                   {playlist.tracks.items.map((item, index) => (
